30142fffd15e33242ae0347494d782f1d381abd2
Ryan Weaver <ryan@thatsquality.com>
2022-11-12 09:52:01 -0500

    [WIP] All Package directory restructure

--- assets/jest.config.js
+++ assets/jest.config.js
@@ -1,5 +1,5 @@
 const { defaults } = require('jest-config');
-const jestConfig = require('../../../../jest.config.js');
+const jestConfig = require('../../../jest.config.js');
 
 jestConfig.moduleFileExtensions = [...defaults.moduleFileExtensions, 'vue'];
 jestConfig.transform['^.+\\.vue$'] = ['@vue/vue3-jest'];
